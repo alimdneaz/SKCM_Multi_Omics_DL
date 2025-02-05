989d27fa7403b572ea99ec612cdbfa3d3f187c81
Ali Neaz <neazali77@gmail.com>
2025-02-05 22:05:02 +0600

    edit rnaseqdataset

--- python_codes/data_setup.py
+++ python_codes/data_setup.py
@@ -1,12 +1,8 @@
-import os
-import torch.nn as nn
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
-NUM_WORKERS = os.cpu_count()
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 
-class RNASeqDataset(nn.Module):
+class RNASeqDataset(Dataset):
     def __init__(self, data, labels):
         self.data = torch.tensor(data, dtype=torch.float32)
         self.labels = torch.tensor(labels, dtype=torch.long)
